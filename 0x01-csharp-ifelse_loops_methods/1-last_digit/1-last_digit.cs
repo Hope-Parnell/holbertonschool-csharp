3f5b1bd2f21f91e0afec506f7ccd0c9014d0bd0e
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-02 12:03:09 -0600

    typo

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -10,7 +10,7 @@ class Program
         if (last > 5)
             Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, last);
         else if (last == 0)
-            Console.WriteLine("The last digit of {0} is {1} and is zero", number, last);
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, last);
         else
             Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last);
     }
