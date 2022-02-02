655fe2086972c24342ff405e2951ed3035e017db
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-02 09:23:28 -0600

    task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last = number % 10;
+        if (last < 0)
+            last *= -1;
+        Console.Write(last);
+        return last;
+    }
+}
