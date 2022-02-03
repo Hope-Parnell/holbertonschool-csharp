3923ae11fe100ef3e2cb8d55351f0e4ad4d45937
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-03 11:11:13 -0600

    task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string best = "None";
+        if (myList != null)
+        {
+            foreach(var entry in myList)
+            {
+                if (best == "None" || entry.Value > myList[best])
+                    best = entry.Key;
+            }
+        }
+        return best;
+    }
+}
