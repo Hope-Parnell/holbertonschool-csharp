bb82623002b7ea3e2eb6c9a32061bfd07ef26198
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-03 10:43:53 -0600

    task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new List<string>();
+        foreach (var item in myDict)
+            keys.Add(item.Key);
+        keys.Sort();
+        keys.ForEach(key => Console.WriteLine("{0}: {1}", key, myDict[key]));
+    }
+}
