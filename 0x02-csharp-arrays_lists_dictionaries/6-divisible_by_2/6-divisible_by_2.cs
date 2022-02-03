20adb28655ef589f71a052d80c642292ab17bb1b
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-03 09:40:37 -0600

    task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> list = new List<bool>();
+        myList.ForEach(n => list.Add(n % 2 == 0));
+        return list;
+    }
+}
