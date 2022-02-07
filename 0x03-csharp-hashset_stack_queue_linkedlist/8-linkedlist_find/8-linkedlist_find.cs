4dc4e1d347b0c86963e2dd43164b161ea6970923
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-07 09:58:23 -0600

    task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int i = 0;
+		foreach (var node in myLList)
+		{
+			if (node == value)
+				return i;
+			i++;
+		}
+		return -1;
+	}
+}
