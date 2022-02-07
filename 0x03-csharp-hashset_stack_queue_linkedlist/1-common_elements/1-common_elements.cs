e70dd644c2ad25d1d1b6bb4e3c7275f8bf1ca5fc
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-07 08:39:23 -0600

    task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+		List<int> commonList = new List<int>();
+		foreach (var n in list1)
+		{
+			if (!commonList.Contains(n) && list2.Contains(n))
+				commonList.Add(n);
+		}
+		commonList.Sort();
+		return commonList;
+	}
+}
