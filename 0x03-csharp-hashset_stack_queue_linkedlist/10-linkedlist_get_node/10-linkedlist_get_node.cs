f0d246c4bca4dd579b6ed9f5e0a45df5bc8e446f
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-07 10:26:21 -0600

    task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		if (n < 0 || n >= myLList.Count)
+			return 0;
+		var node = myLList.First;
+		for (int i = 0; i < n; i++)
+			node = node.Next;
+		return node.Value;
+	}
+}
