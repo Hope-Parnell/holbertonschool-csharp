19b8332308da389dbae3d2e319e5cfe29f067703
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-07 09:21:28 -0600

    task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		// Number of items in queue
+		Console.WriteLine("Number of items: {0}", aQueue.Count);
+		// First item in queue
+		if (aQueue.Count > 0)
+			Console.WriteLine("Top item: {0}", aQueue.Peek());
+		else
+			Console.WriteLine("Queue is empty");
+		// Queue contains <search>
+		Console.WriteLine("Queue contains {0}: {1}", search, aQueue.Contains(search));
+		while (aQueue.Contains(search))
+		{
+			aQueue.Dequeue();
+		}
+		aQueue.Enqueue(newItem);
+		return aQueue;
+	}
+}
