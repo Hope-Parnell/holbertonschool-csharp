577747a5de57140daf77f6266c2dc3f474455b22
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-07 12:40:42 -0600

    added if last case

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -8,6 +8,8 @@ class LList
 		var node = myLList.First;
 		if (node == null)
 			return myLList.AddFirst(n);
+		if (n > myLList.Last.Value)
+			return myLList.AddLast(n);
 		while (node.Next != null && n > node.Next.Value)
 			node = node.Next;
 		return myLList.AddAfter(node, n);
