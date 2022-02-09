1d7eb4029cfa8246e48ea7e5705bf974f1d5108d
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-09 09:35:59 -0600

    task 2 constructor

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,16 @@
+enum Rating {Good, Great, Excellent}
+
+struct Dog {
+	public string name;
+	public float age;
+	public string owner;
+	public Rating rating;
+
+	public Dog(string name, float age, string owner, Rating rating)
+	{
+		this.name = name;
+		this.age = age;
+		this.owner = owner;
+		this.rating = rating;
+	}
+}
