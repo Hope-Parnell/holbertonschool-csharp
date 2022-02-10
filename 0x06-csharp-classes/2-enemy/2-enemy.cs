67c1427cefbe062cabd68a8a4ea4000cd5cfa012
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-10 11:34:32 -0600

    task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Enemies
+{
+	/// <summary>This is the Zombie class.</summary>
+	public class Zombie{
+		/// <summary>Creates public health feild.</summary>
+		public int health;
+		/// <summary>Constructor for Zombie sets health to 0.</summary>
+		public Zombie(){
+			this.health = 0;
+		}
+		/// <summary>
+		/// Constructor for custom Zombie health.
+		/// Throws a ArgumentException error if input is less than 0.
+		/// </summary>
+		public Zombie(int value){
+			if (value < 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+		}
+	}
+}
