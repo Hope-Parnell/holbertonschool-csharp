d365f1b058cb2e279dd142598d02e63b535be47c
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-10 11:27:20 -0600

    task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,12 @@
+namespace Enemies
+{
+	/// <summary>This is the Zombie class.</summary>
+	public class Zombie{
+		/// <summary>Creates public health feild.</summary>
+		public int health;
+		/// <summary>Constructor for Zombie sets health to 0.</summary>
+		public Zombie(){
+			this.health = 0;
+		}
+	}
+}
