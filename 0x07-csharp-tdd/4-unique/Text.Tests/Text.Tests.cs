dd60ae13e6a87a38271d6ba43250606a76326d7d
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-14 14:20:26 -0600

    task 4

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(Str.UniqueChar("test"), 1);
+            Assert.AreEqual(Str.UniqueChar("aabbcc"), -1);
+            Assert.AreEqual(Str.UniqueChar("abcbca"), -1);
+            Assert.AreEqual(Str.UniqueChar("abbcc"), 0);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>Contains methods to be used on strings</summary>
+    public class Str
+    {
+        /// <summary>Returns the index of the first unique character in a string</summary>
+        public static int UniqueChar(string s){
+            for (int i = 0; i < s.Length; i++){
+                int ct = 0;
+                foreach (char c in s){
+                    if (c == s[i])
+                        ct++;
+                    if (ct > 1)
+                        break;
+                }
+                if (ct == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
