308d0b9964c77329080c00d5448f68e4a23bba1a
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-14 11:40:32 -0600

    task 0

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Contains mathmatical operations</summary>
+    public class Operations
+    {
+        /// <summary>Add 2 integers</summary>
+        public static int Add(int a, int b){
+            return a + b;
+        }
+    }
+}
