302fd09bb2741ba37d2a6836eef96604c829b50c
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-15 12:24:44 -0600

    updated documentation

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -5,7 +5,7 @@ namespace MyMath
     /// <summary>Contains mathmatical operations</summary>
     public class Operations
     {
-        /// <summary>Add 2 integers</summary>
+        /// <summary>Add 2 integers.</summary>
         public static int Add(int a, int b){
             return a + b;
         }
