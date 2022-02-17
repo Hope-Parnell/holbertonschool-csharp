099cac8d1e355d868b9fb4a9fea9051c762ad0dc
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-17 12:20:36 -0600

    task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj){
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        var properties = myObj.GetType().GetProperties();
+        foreach(var property in properties){
+            Console.WriteLine(property.Name);
+        }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        var methods = myObj.GetType().GetMethods();
+        foreach(var method in methods){
+            Console.WriteLine(method.Name);
+        }
+    }
+}
