8ab639913683d741bdd0d2035cbe91b07c585294
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-17 11:42:06 -0600

    task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+/// <summary>Contains methods to be used on Obj.</summary>
+class Obj{
+    /// <summary>Returns if the given object is an int</summary>
+	public static bool IsOfTypeInt(object obj) => obj is int;
+}
