6f719e4b5fe793e1338524d563a363e07a8cff36
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-21 13:26:38 -0600

    task 12

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class VectorMath{
+    public static double DotProduct(double[] vector1, double[] vector2){
+        int l1 = vector1.Length;
+        int l2 = vector2.Length;
+        if (l1 != l2 || l1 < 2 || l1 > 3)
+            return -1;
+        double dp = 0;
+        for (int i = 0; i < l1; i++)
+            dp += vector1[i] * vector2[i];
+        return dp;
+    }
+}
