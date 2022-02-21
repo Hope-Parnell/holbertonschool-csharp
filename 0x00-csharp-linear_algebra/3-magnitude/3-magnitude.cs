d315708e43c08bc323f1ab401ead679df8f5545b
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-21 12:12:20 -0600

    task 3

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class VectorMath{
+    /// <summary>Calulates the length of a vector.</summary>
+    public static double Magnitude(double[] vector){
+        if (vector == null || vector.Length < 2 || vector.Length > 3)
+            return -1;
+        double add = 0;
+        foreach(double n in vector){
+            add += n * n;
+        }
+        return Math.Round(Math.Sqrt(add), 2);
+    }
+}
