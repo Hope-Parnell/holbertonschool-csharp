4ad365441d51e3dfc76756374cb932f840f6f16f
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-21 12:49:52 -0600

    task 6

---
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath{
+    public static double[] Add(double[] vector1, double[] vector2){
+        int l1 = vector1.Length;
+        int l2 = vector2.Length;
+        if (l1 != l2 || l1 < 2 || l1 > 3)
+            return new double[] {-1};
+        var list = new List<double>();
+        for (int i = 0; i < l1; i++)
+            list.Add(vector1[i] + vector2[i]);
+        return list.ToArray();
+    }
+}
