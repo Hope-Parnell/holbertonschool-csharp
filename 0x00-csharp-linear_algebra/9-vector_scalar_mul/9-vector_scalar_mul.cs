71d64704cd5c54e98fad3ef48d23806fd29f26bb
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-21 13:06:13 -0600

    task 9

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath{
+    public static double[] Multiply(double[] vector, double scalar){
+        if (vector.Length < 2 || vector.Length > 3)
+            return new double[] {-1};
+        var list = new List<double>();
+        foreach (double n in vector)
+            list.Add(n*scalar);
+        return list.ToArray();
+    }
+}
