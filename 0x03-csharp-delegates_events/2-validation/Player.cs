e3a3387095f85d3a0bc83cb29367c36c97efe9b8
Hope-Parnell <parnell.hope.c@gmail.com>
2022-03-01 12:43:01 -0600

    task 2

---
+++ 0x03-csharp-delegates_events/2-validation/Player.cs
@@ -0,0 +1,46 @@
+using System;
+/// <summary>Delegate for health changes.</summary>
+public delegate void CalculateHealth(float hpInfo);
+/// <summary>Class for a player.</summary>
+public class Player{
+	private string name;
+	private float maxHp;
+	private float hp;
+	/// <summary>Player Constructor sets name and hp.</summary>
+	public Player(string name="Player", float maxHp=100f){
+		this.name = name;
+		if (maxHp <= 0){
+			maxHp = 100f;
+			Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+		}
+		this.maxHp = maxHp;
+		this.hp = maxHp;
+	}
+	/// <summary>Displays current health to the console.</summary>
+	public void PrintHealth(){
+		Console.WriteLine($"{name} has {hp} / {maxHp} health");
+	}
+	/// <summary>Take Damage.</summary>
+	public void TakeDamage(float damage){
+		if (damage < 0)
+			damage = 0f;
+		ValidateHP(this.hp -damage);
+		Console.WriteLine($"{name} takes {damage} damage!");
+	}
+	/// <summary>Heal Damage.</summary>
+	public void HealDamage(float heal){
+		if (heal < 0)
+			heal = 0f;
+		ValidateHP(this.hp + heal);
+		Console.WriteLine($"{name} heals {heal} HP!");
+	}
+	/// <summary>Change current HP to reflect damage/heals.</summary>
+	public void ValidateHP(float newHp){
+		if (newHp < 0)
+			newHp = 0f;
+		if (newHp > this.maxHp)
+			newHp = this.maxHp;
+		this.hp = newHp;
+	}
+}
+
